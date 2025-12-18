05b022617addb43c3ee6c27df0b8ee2dff0e8803
sojanwebyfy <sojanwebyfy@gmail.com>
2025-12-18 14:39:35 +0530

    Deployment testing

--- frontend/components/common/Navbar.js
+++ frontend/components/common/Navbar.js
@@ -115,6 +115,8 @@ export default function Navbar() {
               </Link>
             </div>
 
+
+
             {/* Menu Button (mobile + tablet) */}
             <button
               onClick={() => setOpen(true)}
